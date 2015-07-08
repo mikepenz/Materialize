b7c6a6d476a3a0d7eb4a2acedb0f7541128fc1a0
Mike Penz <mikepenz@gmail.com>
2015-07-08 20:23:03 +0200

    * add AboutLibraries as content to have a bit more real useless

--- app/src/main/java/com/mikepenz/materialize/app/SimpleMaterialize.java
+++ app/src/main/java/com/mikepenz/materialize/app/SimpleMaterialize.java
@@ -1,9 +1,12 @@
 package com.mikepenz.materialize.app;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.aboutlibraries.LibsBuilder;
+import com.mikepenz.aboutlibraries.ui.LibsFragment;
 import com.mikepenz.materialize.MaterializeBuilder;
 
 public class SimpleMaterialize extends AppCompatActivity {
@@ -17,6 +20,11 @@ public class SimpleMaterialize extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         new MaterializeBuilder().withActivity(this).build();
+
+        //init and show about libraries :D
+        LibsFragment fragment = new LibsBuilder().withFields(R.string.class.getFields()).withVersionShown(true).withLicenseShown(true).fragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
     }
 
     @Override
