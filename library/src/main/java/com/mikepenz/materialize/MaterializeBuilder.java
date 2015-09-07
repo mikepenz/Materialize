e1736b2cb12603dc7ae5a114c67f702efebfddf8
Mike Penz <mikepenz@gmail.com>
2015-09-07 21:07:49 +0200

    * extend API with new method to hide the complete systemUI

--- library/src/main/java/com/mikepenz/materialize/MaterializeBuilder.java
+++ library/src/main/java/com/mikepenz/materialize/MaterializeBuilder.java
@@ -255,6 +255,27 @@ public class MaterializeBuilder {
         return this;
     }
 
+
+    // set to no systemUI visible mode
+    protected boolean mSystemUIHidden = false;
+
+    /**
+     * Set to true if you use your app in complete fullscreen mode
+     * with hidden statusBar and navigationBar
+     *
+     * @param systemUIHidden
+     * @return
+     */
+    public MaterializeBuilder withSystemUIHidden(boolean systemUIHidden) {
+        this.mSystemUIHidden = systemUIHidden;
+
+        if (systemUIHidden) {
+            withFullscreen(systemUIHidden);
+        }
+
+        return this;
+    }
+
     // a viewGroup which contains the ScrimInsetsFrameLayout as child
     protected ViewGroup mContainer = null;
 
@@ -329,7 +350,7 @@ public class MaterializeBuilder {
         mScrimInsetsLayout.setTintNavigationBar(mTintNavigationBar);
 
         //if we are fullscreen remove all insets
-        mScrimInsetsLayout.setSystemUIVisible(!mFullscreen);
+        mScrimInsetsLayout.setSystemUIVisible(!mFullscreen && !mSystemUIHidden);
 
         //do some magic specific to the statusBar
         if (!alreadyInflated && mTranslucentStatusBar) {
@@ -365,6 +386,19 @@ public class MaterializeBuilder {
                 if (mTranslucentNavigationBarProgrammatically) {
                     mActivity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
                 }
+            }
+        }
+
+        //set the flags if we want to hide the system ui
+        if (mSystemUIHidden) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                View decorView = this.mActivity.getWindow().getDecorView();
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             }
         }
 
