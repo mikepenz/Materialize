88ffcdfafe47e0989c1ea7684dce9fe32f5583c1
Mike Penz <mikepenz@gmail.com>
2016-01-15 22:16:04 +0100

    * add new useful methds to UIUtils

--- library/src/main/java/com/mikepenz/materialize/util/UIUtils.java
+++ library/src/main/java/com/mikepenz/materialize/util/UIUtils.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
@@ -263,5 +264,78 @@ public class UIUtils {
         iconStateListDrawable.addState(new int[]{android.R.attr.state_selected}, selectedIcon);
         iconStateListDrawable.addState(new int[]{}, icon);
         return iconStateListDrawable;
+    }
+
+
+    /**
+     * helper to get the system default selectable background inclusive an active state
+     *
+     * @param ctx            the context
+     * @param selected_color the selected color
+     * @param animate        true if you want to fade over the states (only animates if API newer than Build.VERSION_CODES.HONEYCOMB)
+     * @return the StateListDrawable
+     */
+    public static StateListDrawable getSelectableBackground(Context ctx, int selected_color, boolean animate) {
+        StateListDrawable states = new StateListDrawable();
+
+        ColorDrawable clrActive = new ColorDrawable(selected_color);
+        states.addState(new int[]{android.R.attr.state_selected}, clrActive);
+
+        states.addState(new int[]{}, ContextCompat.getDrawable(ctx, getSelectableBackground(ctx)));
+        //if possible we enable animating across states
+        if (animate && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            int duration = ctx.getResources().getInteger(android.R.integer.config_shortAnimTime);
+            states.setEnterFadeDuration(duration);
+            states.setExitFadeDuration(duration);
+        }
+        return states;
+    }
+
+    /**
+     * helper to get the system default selectable background inclusive an active and pressed state
+     *
+     * @param ctx            the context
+     * @param selected_color the selected color
+     * @param pressed_alpha  0-255
+     * @param animate        true if you want to fade over the states (only animates if API newer than Build.VERSION_CODES.HONEYCOMB)
+     * @return the StateListDrawable
+     */
+    public static StateListDrawable getSelectablePressedBackground(Context ctx, int selected_color, int pressed_alpha, boolean animate) {
+        StateListDrawable states = getSelectableBackground(ctx, selected_color, animate);
+        ColorDrawable clrPressed = new ColorDrawable(adjustAlpha(selected_color, pressed_alpha));
+        states.addState(new int[]{android.R.attr.state_pressed}, clrPressed);
+        return states;
+    }
+
+    /**
+     * adjusts the alpha of a color
+     *
+     * @param color the color
+     * @param alpha the alpha value we want to set 0-255
+     * @return the adjusted color
+     */
+    public static int adjustAlpha(int color, int alpha) {
+        return (alpha << 24) | (color & 0x00ffffff);
+    }
+
+    /**
+     * helper to get the system default selectable background
+     *
+     * @param ctx
+     * @return
+     */
+    public static int getSelectableBackground(Context ctx) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // If we're running on Honeycomb or newer, then we can use the Theme's
+            // selectableItemBackground to ensure that the View has a pressed state
+            TypedValue outValue = new TypedValue();
+            //it is important here to not use the android.R because this wouldn't add the latest drawable
+            ctx.getTheme().resolveAttribute(R.attr.selectableItemBackground, outValue, true);
+            return outValue.resourceId;
+        } else {
+            TypedValue outValue = new TypedValue();
+            ctx.getTheme().resolveAttribute(android.R.attr.itemBackground, outValue, true);
+            return outValue.resourceId;
+        }
     }
 }
